8ad1ea474f13544016634e06bce11fa3986d7465
Matthew Langlois <fletchto99@gmail.com>
2015-09-18 14:12:07 -0400

    Ability to view collections and categories

--- watchapp/src/js/Apps.js
+++ watchapp/src/js/Apps.js
@@ -21,9 +21,9 @@ Apps.display = function () {
         if (event.itemIndex === 0) {
             displayAllApps(0);
         } else if (event.itemIndex === 1) {
-            //TODO: Categories code
+            displayCategories();
         } else if (event.itemIndex === 2) {
-            //TODO: Collections code
+            displayCollections();
         }
     });
 };
@@ -45,7 +45,7 @@ function displayAllApps(offset) {
             };
         }
         menuItems[data.watchapps.length] = {
-            title:'Load 100 more'
+            title:'Load Next 20'
         };
         var menu = new UI.Menu({
             sections: [{
@@ -59,4 +59,128 @@ function displayAllApps(offset) {
         menu.show();
     };
     functions.apiCall('Populating watchapps',data,onSuccess);
+}
+
+
+function displayCategories() {
+    var data = {
+        method: 'app_categories',
+        platform: 'basalt'
+    };
+    var onSuccess = function(data) {
+        var menuItems = [data.categories.length];
+        for (var i = 0; i < data.categories.length; i++) {
+            menuItems[i] = {
+                title: data.categories[i].name,
+                slug: data.categories[i].slug
+            };
+        }
+        var menu = new UI.Menu({
+            sections: [{
+                title: 'App Categories',
+                items: menuItems
+            }]
+        });
+        menu.on('select', function(event) {
+            displayAppsInCategory(event.item.slug, 0);
+        });
+        menu.show();
+    };
+    functions.apiCall('Populating categories',data,onSuccess);
+}
+
+function displayAppsInCategory(category, offset) {
+    var data = {
+        method: 'apps_in_category',
+        platform: 'basalt',
+        category: category,
+        offset: offset
+    };
+    var onSuccess = function(data) {
+        var menuItems = [data.watchapps.length + 1];
+        for (var i = 0; i < data.watchapps.length; i++) {
+            menuItems[i] = {
+                title: data.watchapps[i].title,
+                subtitle: 'By ' + data.watchapps[i].author,
+                id: data.watchapps[i].id,
+                icon: data.watchapps[i].icon_image['28x28']
+            };
+        }
+        menuItems[data.watchapps.length] = {
+            title:'Load Next 20'
+        };
+        var menu = new UI.Menu({
+            sections: [{
+                title: 'All Apps',
+                items: menuItems
+            }]
+        });
+        menu.on('select', function(event) {
+            console.log(event.item.icon);
+        });
+        menu.show();
+    };
+    functions.apiCall('Populating apps',data,onSuccess);
+}
+
+
+function displayCollections() {
+    var data = {
+        method: 'app_collections',
+        platform: 'basalt'
+    };
+    var onSuccess = function(data) {
+        var menuItems = [data.collections.length];
+        for (var i = 0; i < data.collections.length; i++) {
+            menuItems[i] = {
+                title: data.collections[i].name,
+                slug: data.collections[i].slug
+            };
+        }
+        var menu = new UI.Menu({
+            sections: [{
+                title: 'App Collections',
+                items: menuItems
+            }]
+        });
+        menu.on('select', function(event) {
+            displayAppsInCollection(event.item.slug, 0);
+        });
+        menu.show();
+    };
+    functions.apiCall('Populating collections',data,onSuccess);
+}
+
+function displayAppsInCollection(collection, offset) {
+    var data = {
+        method: 'faces_in_collection',
+        platform: 'basalt',
+        collection: collection,
+        offset: offset
+    };
+    var onSuccess = function(data) {
+        var menuItems = [data.watchapps.length + 1];
+        for (var i = 0; i < data.watchapps.length; i++) {
+            menuItems[i] = {
+                title: data.watchapps[i].title,
+                subtitle: 'By ' + data.watchapps[i].author,
+                id: data.watchapps[i].id,
+                icon: data.watchapps[i].icon_image['28x28']
+            };
+        }
+        menuItems[data.watchapps.length] = {
+            title:'Load Next 20'
+        };
+        var menu = new UI.Menu({
+            sections: [{
+                title: 'All Apps',
+                items: menuItems
+            }]
+        });
+        menu.on('select', function(event) {
+            console.log(event.item.icon);
+        });
+        menu.show();
+    };
+    functions.apiCall('Populating apps',data,onSuccess);
 }
--- watchapp/src/js/Faces.js
+++ watchapp/src/js/Faces.js
@@ -19,6 +19,7 @@ Faces.display = function () {
         if (event.itemIndex === 0) {
             displayAllFaces(0);
         } else if (event.itemIndex === 1) {
+            displayCollections(0);
         }
     });
 };
@@ -30,6 +31,89 @@ function displayAllFaces(offset) {
         offset: offset
     };
     var onSuccess = function(data) {
+        var menuItems = [data.watchapps.length + 1];
+        for (var i = 0; i < data.watchapps.length; i++) {
+            menuItems[i] = {
+                title: data.watchapps[i].title,
+                subtitle: 'By ' + data.watchapps[i].author,
+                id: data.watchapps[i].id,
+                icon: data.watchapps[i].icon_image['28x28']
+            };
+        }
+        menuItems[data.watchapps.length] = {
+            title:'Load Next 20'
+        };
+        var menu = new UI.Menu({
+            sections: [{
+                title: 'All Apps',
+                items: menuItems
+            }]
+        });
+        menu.on('select', function(event) {
+            console.log(event.item.icon);
+        });
+        menu.show();
     };
     functions.apiCall('Populating watchfaces',data,onSuccess);
+}
+
+function displayCollections() {
+    var data = {
+        method: 'face_collections',
+        platform: 'basalt'
+    };
+    var onSuccess = function(data) {
+        var menuItems = [data.collections.length];
+        for (var i = 0; i < data.collections.length; i++) {
+            menuItems[i] = {
+                title: data.collections[i].name,
+                slug: data.collections[i].slug
+            };
+        }
+        var menu = new UI.Menu({
+            sections: [{
+                title: 'Face Collections',
+                items: menuItems
+            }]
+        });
+        menu.on('select', function(event) {
+            displayFacesInCollection(event.item.slug, 0);
+        });
+        menu.show();
+    };
+    functions.apiCall('Populating collections',data,onSuccess);
+}
+
+function displayFacesInCollection(collection, offset) {
+    var data = {
+        method: 'faces_in_collection',
+        platform: 'basalt',
+        collection: collection,
+        offset: offset
+    };
+    var onSuccess = function(data) {
+        var menuItems = [data.watchapps.length + 1];
+        for (var i = 0; i < data.watchapps.length; i++) {
+            menuItems[i] = {
+                title: data.watchapps[i].title,
+                subtitle: 'By ' + data.watchapps[i].author,
+                id: data.watchapps[i].id,
+                icon: data.watchapps[i].icon_image['28x28']
+            };
+        }
+        menuItems[data.watchapps.length] = {
+            title:'Load Next 20'
+        };
+        var menu = new UI.Menu({
+            sections: [{
+                title: 'All Apps',
+                items: menuItems
+            }]
+        });
+        menu.on('select', function(event) {
+            console.log(event.item.icon);
+        });
+        menu.show();
+    };
+    functions.apiCall('Populating apps',data,onSuccess);
 }
